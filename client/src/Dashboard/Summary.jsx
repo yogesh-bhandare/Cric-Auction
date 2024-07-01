bd459adea53ac5b47557f8b4c0f5ba65237f89a4
gityb8 <statichub8@gmail.com>
2024-07-01 21:33:48 +0530

    Player List Complete

--- client/src/Dashboard/Summary.jsx
+++ client/src/Dashboard/Summary.jsx
@@ -1,179 +1,6 @@
-// import React, { useState, useEffect } from "react";
-// import { NavLink } from "react-router-dom";
-// import AxiosInstance from "../Axios";
-
-// const Summary = () => {
-//   const [activeTab, setActiveTab] = useState("teams");
-//   const [auctionResults, setAuctionResults] = useState([]);
-//   const [players, setPlayers] = useState([]);
-//   const [teams, setTeams] = useState([]);
-
-//   useEffect(() => {
-//     const fetchSummaryData = async () => {
-//       try {
-//         const response = await AxiosInstance.get("/summary/");
-//         console.log("Fetched auction results:", response.data);
-
-//         // Fetch player and team data
-//         const playersData = await Promise.all(response.data.map(async (result) => {
-//           // Fetch player details
-//           const playerResponse = await AxiosInstance.get(`/players/${result.player}/`);
-//           const playerData = playerResponse.data;
-
-//           // Fetch team details if team is not null
-//           let teamData = {};
-//           if (result.team !== null) {
-//             const teamResponse = await AxiosInstance.get(`/teams/${result.team}/`);
-//             teamData = { [result.team]: teamResponse.data };
-//           }
-
-//           return {
-//             id: result.player.id,
-//             name: playerData.player_name,
-//             type: playerData.player_type,
-//             points: playerData.player_points,
-//             origin: playerData.origin,
-//             base_price: playerData.base_price,
-//             status: result.status,
-//             price: result.sold_price,
-//             team: result.team ? teamData[result.team].team_name : "Unknown Team",
-//           };
-//         }));
-
-//         console.log("Mapped players data:", playersData);
-//         setPlayers(playersData);
-
-//         const teamsData = [...new Set(response.data.map((result) => (result.team ? result.team.team_name : "Unknown Team")))];
-//         console.log("Mapped teams data:", teamsData);
-//         setTeams(teamsData);
-
-//         setAuctionResults(response.data);
-//       } catch (error) {
-//         console.error("Error fetching auction results:", error);
-//       }
-//     };
-
-//     fetchSummaryData();
-//   }, []);
-
-//   useEffect(() => {
-//     console.log("Players data:", players);
-//     console.log("Teams data:", teams);
-//   }, [players, teams]);
-
-//   const teamData = teams.map((team) => ({
-//     name: team,
-//     players: players.filter((player) => player.team === team),
-//   }));
-
-//   return (
-//     <div className="w-full h-screen p-8 bg-[#262626] text-white flex flex-col items-center">
-//       <div className="absolute top-8 left-8">
-//         <NavLink
-//           to="/auction/lists"
-//           className="py-2 px-4 bg-[#F23D4C] text-white font-semibold rounded hover:bg-[#BFF207] hover:text-[#262626] transition-colors duration-300"
-//         >
-//           Back
-//         </NavLink>
-//       </div>
-//       <h1 className="text-4xl font-bold text-[#F23D4C] mb-8">Auction Summary</h1>
-//       <div className="flex mb-8">
-//         <button
-//           onClick={() => setActiveTab("teams")}
-//           className={`py-2 px-4 ${activeTab === "teams" ? "bg-[#F23D4C]" : "bg-gray-700"} text-white font-semibold rounded-l hover:bg-[#BFF207] hover:text-[#262626] transition-colors duration-300`}
-//         >
-//           Team List
-//         </button>
-//         <button
-//           onClick={() => setActiveTab("players")}
-//           className={`py-2 px-4 ${activeTab === "players" ? "bg-[#F23D4C]" : "bg-gray-700"} text-white font-semibold rounded-r hover:bg-[#BFF207] hover:text-[#262626] transition-colors duration-300`}
-//         >
-//           Player List
-//         </button>
-//       </div>
-//       {activeTab === "teams" ? (
-//         <div className="overflow-x-auto w-full">
-//           {teamData.length > 0 ? (
-//             teamData.map((team, index) => (
-//               <div key={index} className="mb-8 w-full">
-//                 <h2 className="text-3xl font-bold text-[#BFF207] mb-4">{team.name}</h2>
-//                 {team.players.length > 0 ? (
-//                   <table className="min-w-full bg-white rounded-lg shadow-lg mb-4">
-//                     <thead className="bg-[#F23D4C] text-white">
-//                       <tr>
-//                         <th className="py-3 px-6 text-left">Player Name</th>
-//                         <th className="py-3 px-6 text-left">Type</th>
-//                         <th className="py-3 px-6 text-left">Points</th>
-//                         <th className="py-3 px-6 text-left">Origin</th>
-//                         <th className="py-3 px-6 text-left">Base Price</th>
-//                         <th className="py-3 px-6 text-left">Price</th>
-//                       </tr>
-//                     </thead>
-//                     <tbody className="text-gray-600">
-//                       {team.players.map((player) => (
-//                         <tr key={player.id} className="border-b border-gray-200 hover:bg-gray-100">
-//                           <td className="py-3 px-6 text-left">{player.name}</td>
-//                           <td className="py-3 px-6 text-left">{player.type}</td>
-//                           <td className="py-3 px-6 text-left">{player.points}</td>
-//                           <td className="py-3 px-6 text-left">{player.origin}</td>
-//                           <td className="py-3 px-6 text-left">{player.base_price}</td>
-//                           <td className="py-3 px-6 text-left">{player.price}</td>
-//                         </tr>
-//                       ))}
-//                     </tbody>
-//                   </table>
-//                 ) : (
-//                   <p className="text-xl text-white">No players bought by this team.</p>
-//                 )}
-//               </div>
-//             ))
-//           ) : (
-//             <p className="text-xl text-white">No team data available.</p>
-//           )}
-//         </div>
-//       ) : (
-//         <div className="overflow-x-auto w-full">
-//           <table className="min-w-full bg-white rounded-lg shadow-lg">
-//             <thead className="bg-[#F23D4C] text-white">
-//               <tr>
-//                 <th className="py-3 px-6 text-left">Player Name</th>
-//                 <th className="py-3 px-6 text-left">Player Type</th>
-//                 <th className="py-3 px-6 text-left">Player Points</th>
-//                 <th className="py-3 px-6 text-left">Origin</th>
-//                 <th className="py-3 px-6 text-left">Base Price</th>
-//                 <th className="py-3 px-6 text-left">Sold Price</th>
-//                 <th className="py-3 px-6 text-left">Status</th>
-//                 <th className="py-3 px-6 text-left">Team Name</th>
-//               </tr>
-//             </thead>
-//             <tbody className="text-gray-600">
-//               {players.map((player) => (
-//                 <tr key={player.id} className="border-b border-gray-200 hover:bg-gray-100">
-//                   <td className="py-3 px-6 text-left">{player.name}</td>
-//                   <td className="py-3 px-6 text-left">{player.type}</td>
-//                   <td className="py-3 px-6 text-left">{player.points}</td>
-//                   <td className="py-3 px-6 text-left">{player.origin}</td>
-//                   <td className="py-3 px-6 text-left">{player.base_price}</td>
-//                   <td className="py-3 px-6 text-left">{player.price ? player.price : "Null"}</td>
-//                   <td className="py-3 px-6 text-left">{player.status}</td>
-//                   {/* {teams.map((team) => (
-//                     <td className="py-3 px-6 text-left">{team.team_name}</td>
-//                   ))} */}
-//                 </tr>
-//               ))}
-//             </tbody>
-//           </table>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Summary;
-
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
-import AxiosInstance from "../Axios"; // Adjust the import based on your file structure
+import AxiosInstance from "../Axios"; 
 
 const Summary = () => {
   const [activeTab, setActiveTab] = useState("teams");
@@ -268,7 +95,7 @@ const Summary = () => {
               <div key={index} className="mb-8 w-full">
                 <h2 className="text-3xl font-bold text-[#BFF207] mb-4">{team.name}</h2>
                 {team.players.length > 0 ? (
-                  <table className="min-w-full bg-white rounded-lg shadow-lg mb-4">
+                  <table className="min-w-full bg-white shadow-xl mb-4">
                     <thead className="bg-[#F23D4C] text-white">
                       <tr>
                         <th className="py-3 px-6 text-left">Player Name</th>
@@ -303,7 +130,7 @@ const Summary = () => {
         </div>
       ) : (
         <div className="overflow-x-auto w-full">
-          <table className="min-w-full bg-white rounded-lg shadow-lg">
+          <table className="min-w-full bg-white shadow-xl">
             <thead className="bg-[#F23D4C] text-white">
               <tr>
                 <th className="py-3 px-6 text-left">Player Name</th>
@@ -311,9 +138,9 @@ const Summary = () => {
                 <th className="py-3 px-6 text-left">Player Points</th>
                 <th className="py-3 px-6 text-left">Origin</th>
                 <th className="py-3 px-6 text-left">Base Price</th>
-                <th className="py-3 px-6 text-left">Price</th>
                 <th className="py-3 px-6 text-left">Status</th>
                 <th className="py-3 px-6 text-left">Team Name</th>
+                <th className="py-3 px-6 text-left">Sold Price</th>
               </tr>
             </thead>
             <tbody className="text-gray-600">
@@ -324,8 +151,9 @@ const Summary = () => {
                   <td className="py-3 px-6 text-left">{player.points}</td>
                   <td className="py-3 px-6 text-left">{player.origin}</td>
                   <td className="py-3 px-6 text-left">{player.base_price}</td>
-                  <td className="py-3 px-6 text-left">{player.price ? player.price : "NULL"}</td>
                   <td className="py-3 px-6 text-left">{player.status}</td>
+                  <td className="py-3 px-6 text-left">{player.team}</td>
+                  <td className="py-3 px-6 text-left">{player.price ? player.price : "NULL"}</td>
                 </tr>
               ))}
             </tbody>
